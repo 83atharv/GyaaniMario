67930add3452e0467265bd5549e9d9ea295c24a6
83atharv <atharvdixit06@gmail.com>
2024-12-22 00:25:26 +0530

    made minor changes

--- html/finance-script.js
+++ html/finance-script.js
@@ -21,14 +21,12 @@ levels.forEach(level => {
 });
 
 
-// Check if dark mode preference is saved in localStorage
 window.addEventListener('load', function () {
     if (localStorage.getItem('darkMode') === 'enabled') {
         document.body.classList.add('dark-mode');
     }
 });
 
-// Toggle dark mode
 function toggleDarkMode() {
     const isDarkMode = document.body.classList.toggle('dark-mode');
     if (isDarkMode) {
